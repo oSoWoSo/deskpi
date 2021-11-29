b80dc19c75a61fd1a76dd970230266ce415f2eb0
zenobit <6384793+zen0bit@users.noreply.github.com>
2021-11-29 11:33:22 +0100

    Update and rename uninstall-fedora.sh to uninstall-void.sh

--- uninstall-void.sh
+++ uninstall-void.sh
@@ -2,9 +2,9 @@
 # uninstall deskpi script 
 . /lib/lsb/init-functions
 
-daemonname="deskpi"
-deskpidaemon=/lib/systemd/system/$daemonname.service
-safeshutdaemon=/lib/systemd/system/$daemonname-safeshut.service
+daemonname="deskpid"
+deskpidaemon=/etc/sv/$daemonname
+safeshutdaemon=/etc/sv/$daemonname-safeshut
 
 echo "Uninstalling DeskPi PWM Fan Control and Safeshut Service."
 sleep 1
@@ -12,10 +12,10 @@ echo "Disable DeskPi PWM Fan Control and Safeshut Service."
 echo "Remove dtoverlay configure from /boot/efi/config.txt file"
 sudo sed -i '/dtoverlay=dwc2,dr_mode=host/d' /boot/efi/config.txt
 echo "Stop and disable DeskPi services"
-sudo systemctl disable $daemonname.service 2&>/dev/null  
-sudo systemctl stop $daemonname.service  2&>/dev/null
-sudo systemctl disable $daemonname-safeshut.service 2&>/dev/null
-sudo systemctl stop $daemonname-safeshut.service 2&>/dev/null
+sudo sv disable $daemonname.service 2&>/dev/null  
+sudo sv stop $daemonname.service  2&>/dev/null
+sudo sv disable $daemonname-safeshut.service 2&>/dev/null
+sudo sv stop $daemonname-safeshut.service 2&>/dev/null
 echo "Remove DeskPi PWM Fan Control and Safeshut Service."
 sudo rm -f  $deskpidaemon  2&>/dev/null 
 sudo rm -f  $safeshutdaemon 2&>/dev/null 
